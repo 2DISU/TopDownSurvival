960ac8eb9bc2a7ba2f40c26018cd2d80d0fe0176
DanielWWallace <44872880+DanielWWallace@users.noreply.github.com>
2018-11-23 09:57:26 -0500

    Update background.java

--- background.java
+++ background.java
@@ -8,23 +8,16 @@ import javax.imageio.ImageIO;
 
 
 public class background {
-	public BufferedImage tile1,tile2,tile3,tile4,tile7 = null;
+	public BufferedImage[] tile =new BufferedImage[17];
 	public int xOffset=-85;
 	public int yOffset=-50;
 	public int h=256;//tile height
 	public background()
 	{
 		try {
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile2 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_2.png"));
-			tile3 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_3.png"));
-			tile4 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_4.png"));
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile1 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_1.png"));
-			tile7 = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_7.png"));
+			for (int i=0;i<=tile.length;i++) {
+			tile[i] = ImageIO.read(new File("\\\\SS08\\S231 Students$\\S201060117\\Documents\\GitHub\\TopDownSurvival\\assets\\desert_top_down\\images\\Tile_"+i+".png"));
+			}
 		} catch (IOException e) {
 			System.out.println("No Image");
 		}
@@ -32,10 +25,10 @@ public class background {
 	public void paint(Graphics2D g)
 	{	
 		
-		g.drawImage(tile1,0+xOffset,0+yOffset, null);
-		g.drawImage(tile4,0+xOffset,h+yOffset, null);
-		g.drawImage(tile4,0+xOffset,2*h+yOffset, null);
-		g.drawImage(tile7,0+xOffset,3*h+yOffset, null);
-		
+		g.drawImage(tile[0],0+xOffset,0+yOffset, null);
+		g.drawImage(tile[3],0+xOffset,h+yOffset, null);
+		g.drawImage(tile[3],0+xOffset,2*h+yOffset, null);
+		g.drawImage(tile[6],0+xOffset,3*h+yOffset, null);
+		//g.drawImage();
 	}
 }
